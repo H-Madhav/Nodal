ba0188a172147df3b9a0682fe28bca8411717038
Hari Madhav Jha <madhav1993hari@gmail.com>
2017-04-04 04:36:42 -0700

    Add ngRoute

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+
+angular.module('nodal', ["ngRoute"])
+.config(function($routeProvider) {
+    
+    $routeProvider
+    .when("/", {
+    	templateUrl : "templates/index.html"
+    })
+    .when("/features", {
+    	templateUrl : "templates/features.html"
+    })
+    .when("/contactUs", {
+    	templateUrl : "templates/contact.html"
+    });
+});
+
+
 function openNav() {
     document.getElementById("mySidenav").style.left = "0";
 }
@@ -5,3 +22,4 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.left = "-999px";
 }
+
