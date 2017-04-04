068795bcd4654f4dd687eaefaf24f3b8d04d5a7b
Hari Madhav Jha <madhav1993hari@gmail.com>
2017-04-04 05:37:49 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,4 @@
 
-angular.module('nodal', ["ngRoute"])
-.config(function($routeProvider) {
-    
-    $routeProvider
-    .when("/", {
-    	templateUrl : "templates/index.html"
-    })
-    .when("/features", {
-    	templateUrl : "templates/features.html"
-    })
-    .when("/contactUs", {
-    	templateUrl : "templates/contact.html"
-    });
-});
-
-
 function openNav() {
     document.getElementById("mySidenav").style.left = "0";
 }
