3e9c36a7e5e8ddda10cb3624de37641770600ad3
Hari Madhav Jha <madhav1993hari@gmail.com>
2017-04-05 05:35:18 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,3 +7,20 @@ function closeNav() {
     document.getElementById("mySidenav").style.left = "-999px";
 }
 
+
+angular.module("nodal", ["ngRoute"])
+.config(function($routeProvider){
+	$routeProvider
+	.when("/", {
+		templateUrl : "Contact/contactIndex.html"
+	})
+	.when("/form", {
+	    templateUrl : "Contact/contactForm.html"	
+	})
+	.when("/thankU", {
+	    templateUrl : "Contact/thankU.html"	
+	})
+	.when("/contact.html", {
+		templateUrl : "Contact/contactIndex.html"
+	});
+});
